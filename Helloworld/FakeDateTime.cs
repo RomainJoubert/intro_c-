cbd1d1358838e4b27bf895763f83f09b747f59da
RomainJoubert <romain.joubert@Digital-Grenoble.local>
2018-08-21 17:10:37 +0200

    tests pour le soir, weekend...

--- Helloworld/FakeDateTime.cs
+++ Helloworld/FakeDateTime.cs
@@ -6,13 +6,29 @@ using System.Threading.Tasks;
 
 namespace Helloworld
 {
+    //public class FakeDateTime : IDateTime
+    //{
+    //    public DateTime Date
+    //    {
+    //        get
+    //        {
+    //            return new DateTime(2018, 08, 25, 20, 00, 00);
+    //        }
+    //    }
+    //}
+
     public class FakeDateTime : IDateTime
     {
+        DateTime _dateToReturn;
+        public FakeDateTime(DateTime dateToReturn)
+        {
+            _dateToReturn = dateToReturn;
+        }
         public DateTime Date
         {
             get
             {
-                return new DateTime(2018, 08, 25, 20, 00, 00);
+                return _dateToReturn;
             }
         }
     }
--- HelloworldTests/MessageTests.cs
+++ HelloworldTests/MessageTests.cs
@@ -11,67 +11,47 @@ namespace Helloworld.Tests
     [TestClass()]
     public class MessageTests
     {
-        //[TestMethod()]
-        //public void MessageTest()
-        //{
-        //}
+        [TestMethod()]
+        public void MessageTest()
+        {
+        }
 
-        //[TestMethod()]
-        //public void getHelloMessageTest_bonsoir()
-        //{
-        //    //Etant donné l'instanciation d'un nouveau message
-        //    Message test = new Message(9, 13, 18);
+        [TestMethod()]
+        public void getHelloMessageTest_bonsoir()
+        {
+            DateTime date = new DateTime(2018, 08, 23, 19, 00, 00);
+            Message test = new Message(new FakeDateTime(date));
+            String result = test.getHelloMessage();
+            Assert.IsTrue(result.Contains("Bonsoir"));
 
-        //    //Lorsque qu'on appelle getHelloMessage et que j'instancie une nouvelle date avec comme horaire 20h
-        //    DateTime date = new DateTime(2018, 08, 23, 20, 00, 00);
-        //    String result = test.getHelloMessage(date);
+        }
 
-        //    //Alors "Bonsoir" s'affiche
-        //    Assert.IsTrue(result.Contains("Bonsoir"));
+        [TestMethod()]
+        public void getHelloMessageTest_bonjour()
+        {
+            DateTime date = new DateTime(2018, 08, 23, 10, 00, 00);
+            Message test = new Message(new FakeDateTime(date));
+            String result = test.getHelloMessage();
+            Assert.IsTrue(result.Contains("Bonjour"));
+        }
 
-        //}
+        [TestMethod()]
+        public void getHelloMessageTest_bonaprem()
+        {
+            DateTime date = new DateTime(2018, 08, 23, 15, 00, 00);
+            Message test = new Message(new FakeDateTime(date));
+            String result = test.getHelloMessage();
+            Assert.IsTrue(result.Contains("Bon après-midi"));
 
-        //[TestMethod()]
-        //public void getHelloMessageTest_bonjour()
-        //{
-        //    //Etant donné l'instanciation d'un nouveau message
-        //    Message test = new Message(9, 13, 18);
-
-        //    //Lorsque qu'on appelle getHelloMessage et que j'instancie une nouvelle date avec comme horaire 10h
-        //    DateTime date = new DateTime(2018, 08, 23, 10, 00, 00);
-        //    String result = test.getHelloMessage(date);
-
-        //    //Alors "Bonjour" s'affiche
-        //    Assert.IsTrue(result.Contains("Bonjour"));
-        //}
-
-        //[TestMethod()]
-        //public void getHelloMessageTest_bonaprem()
-        //{
-        //    //Etant donné l'instanciation d'un nouveau message
-        //    Message test = new Message(9, 13, 18);
-
-        //    //Lorsque qu'on appelle getHelloMessage et que j'instancie une nouvelle date avec comme horaire 15h
-        //    DateTime date = new DateTime(2018, 08, 23, 15, 00, 00);
-        //    String result = test.getHelloMessage(date);
-
-        //    //Alors "Bonjour" s'affiche
-        //    Assert.IsTrue(result.Contains("Bon après-midi"));
-
-        //}
+        }
 
 
         [TestMethod()]
         public void getHelloMessageTest_weekend()
         {
-            //Etant donné l'instanciation d'un nouveau message
-            Message test = new Message(new FakeDateTime());
-
-            //Lorsque qu'on appelle getHelloMessage et que j'instancie une nouvelle date avec comme horaire 10h mais un samedi
-            
+            DateTime date = new DateTime(2018, 08, 27, 08, 59, 00);
+            Message test = new Message(new FakeDateTime(date));
             String result = test.getHelloMessage();
-
-            //Alors "Bonjour" s'affiche
             Assert.IsTrue(result.Contains("Bon weekend"));
 
         }
